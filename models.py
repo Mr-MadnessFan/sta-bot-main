14e6dbe09991d2d040910421c83f1319cb13e487
Mr-MadnessFan <nsakura5472@gmail.com>
2025-10-03 14:28:46 +0500

    Omega

---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String, nullable=False)
